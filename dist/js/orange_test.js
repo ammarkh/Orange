357ae0675d579dd727c269ec34dd1afbda8aa5ed
ammar kh <a.m.k-it@hotmail.com>
2018-02-14 01:49:45 +0200

    create v1 from popupmodel

--- dist/js/orange_test.js
+++ dist/js/orange_test.js
@@ -1,21 +1,45 @@
 (function(window){
     var Orange = function (){
-      
-    }
+      document.createElement('button');
+    };
 
     Orange.prototype ={
              /** function for on / off Model   */
 
-            createModel:function(){
-                var ModelContent = document.createElement('div');
-                ModelContent.setAttribute('class', 'content-model');
-                var model = document.createElement('div').setAttribute('class', 'model');
-                var modelTitle = document.createElement('div').setAttribute('class', 'model-title info');
-                var modelOff = document.createElement('span').setAttribute('class', 'model-off-icon');
-                modelOff+= '&#9932;';
+            createModel:function(title, message){
+              // debugger;
+                var modelOffCtrl = document.createElement('span');
+                    modelOffCtrl.setAttribute('class','model-off-icon');
+                    modelOffCtrl.innerHTML = "&#9932;";
+            
+           
+                var modelTitle = document.createElement('div');
+            
+                modelTitle.appendChild(modelOffCtrl);
+                
+                if(title){
+                modelTitle.setAttribute('class','model-title');
+                var modelTitleText = document.createElement('label');
+                modelTitleText.innerText = title;       //check type of title 
+                modelTitle.appendChild(modelTitleText);
+                }
+
+                var model = document.createElement('div');
+                model.setAttribute('class','model');
+                model.appendChild(modelTitle);
+
+                var modelContent = document.createElement('div');
+                modelContent.setAttribute('class','content-model');
+                modelContent.appendChild(model);
+             
+                document.body.appendChild(modelContent);
+
+                
+                return model;
+
             },
-            viewData:function(){
-                console.log('da');
+            viewData:function(obj){
+               console.log(typeof obj);
             },
             onModel : function (){
 
@@ -26,6 +50,6 @@
 
     }
 
-    window.Orange = Orange ;
-
+    window.Orange = Orange.prototype ;
+    return Orange;
 })(window);
