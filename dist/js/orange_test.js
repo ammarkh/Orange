4942b616ef9e7872ae3382e0ed757709945221c4
ammar kh <a.m.k-it@hotmail.com>
2018-02-14 02:36:37 +0200

    model v1

--- dist/js/orange_test.js
+++ dist/js/orange_test.js
@@ -11,25 +11,34 @@
                 var modelOffCtrl = document.createElement('span');
                     modelOffCtrl.setAttribute('class','model-off-icon');
                     modelOffCtrl.innerHTML = "&#9932;";
-            
+                    modelOffCtrl.addEventListener('click',this.popupOff, false);
            
                 var modelTitle = document.createElement('div');
             
                 modelTitle.appendChild(modelOffCtrl);
                 
                 if(title){
-                modelTitle.setAttribute('class','model-title');
+                modelTitle.setAttribute('class','model-title info');
                 var modelTitleText = document.createElement('label');
                 modelTitleText.innerText = title;       //check type of title 
                 modelTitle.appendChild(modelTitleText);
                 }
 
+              
                 var model = document.createElement('div');
-                model.setAttribute('class','model');
+                model.setAttribute('class','model success');
                 model.appendChild(modelTitle);
+
+                if(message){
+                    var MessageElm = document.createElement('label');
+                    MessageElm.innerText = message;
+
+                    model.appendChild(MessageElm);
+                }
 
                 var modelContent = document.createElement('div');
                 modelContent.setAttribute('class','content-model');
+                modelContent.setAttribute('style','display:none');
                 modelContent.appendChild(model);
              
                 document.body.appendChild(modelContent);
@@ -41,15 +50,21 @@
             viewData:function(obj){
                console.log(typeof obj);
             },
-            onModel : function (){
-
+            Model:function(title, Msg){
+                this.createModel(title,Msg);
             },
-            OffModel :function(){
-
+            popup : function (){
+                var modelContent = document.getElementsByClassName('content-model')[0];
+                modelContent.removeAttribute('style');
+               
+            },
+            popupOff :function(){
+                var modelContent = document.getElementsByClassName('content-model')[0];
+                modelContent.setAttribute('style', 'display:none');
             }
 
     }
 
     window.Orange = Orange.prototype ;
-    return Orange;
+    return Orange;  //change this module to orange.model and make module for each style.
 })(window);
