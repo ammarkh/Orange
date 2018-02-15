6780d2e5a348ebe3f0a425a4fd2161370b443569
ammar kh <a.m.k-it@hotmail.com>
2018-02-15 15:57:10 +0200

    new design for library

--- dist/js/Orange.js
+++ dist/js/Orange.js
@@ -36,4 +36,4 @@ var Orange = window.Orange;
 
 var O = window.Orange;
 
-/**this library will content text, div, button, model , responsive , navbar */
+/**this library will content text, div, button, model , responsive , navbar, check box, radio button, socity icon, alertMessage, box, background, validation, chart , form  */
