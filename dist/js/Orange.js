15e0026437eaac8c89dfb112af3892dfb6449fd0
ammar kh <a.m.k-it@hotmail.com>
2018-02-17 19:44:46 +0200

    new elements with library file has created

--- dist/js/Orange.js
+++ dist/js/Orange.js
@@ -34,28 +34,44 @@
         this.el = el ;
     };
 
+    //model function 
     Orange.prototype.model  = function (el){
         //body
-            this.model = window.model(el);
+            this.model = window.model(this.el);
+        return this;
+    };
+
+    Orange.prototype.message = function(el){
+        //body
+        this.message = window.message(this.el);
         return this;
     };
 
     Orange.prototype.checkParamType = function (){
-        if(typeof el === 'undefined'){
+        if(typeof this.el === 'undefined'){
+            
             console.log('error : cannot use Orange with no element ');
+
+            //throw error invalid param.
         }
 
         if (typeof this.el === 'string'){       // function for check if param string 
             if(this.el.toString().startsWith('#'))// function for id 
                 console.log('id'+ el);
             else if(this.el.toString().startsWith('.')){  // check for class 
-                console.log('class parameter ' + el);
+               // console.log('class parameter ' + el);
+               var allELementsClass = document.getElementsByClassName(el);
             }
             else if (this.el.toString().startsWith('[') && this.el.toString().endsWith(']')){
-                console.log(this.el.toString().substr(this.el.toString().startsWith(':'), this.el.toString().length));
-                console.log('tag parameter ');
+                
+               var tagName =this.el.toString().trim().substring(this.el.indexOf(':')+1, this.el.length-1);
+                
+               // console.log(tagName);
             }
+        } else {
+            console.log(this.el);
         }
+     
     };
 
      return new Orange(el);
