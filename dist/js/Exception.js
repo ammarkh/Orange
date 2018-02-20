8238eea1efb9372bbf335fbf3c67903d520a6bef
ammar kh <a.m.k-it@hotmail.com>
2018-02-20 19:45:29 +0200

    start validation v1

--- dist/js/Exception.js
+++ dist/js/Exception.js
@@ -1,16 +1,19 @@
-(function(){
+(function() {
     //exception number
-    var Exception = (function(){
+    var Exception = (function() {
 
         const err1 = '';
         const err2 = '';
-        function Exception(){
-            return new Exception();
+        const err3 = '';
+        const err4 = '';
+
+        function Exception(number, message) {
+
         }
 
-        
 
-            return Exception;
+
+        return Exception;
     })();
-   
+
 })();
--- dist/js/Orange.js
+++ dist/js/Orange.js
@@ -24,7 +24,7 @@ function Orange  (message){
 
  */
 
-var Orange = function(el) {
+O = Orange = function(el) {
 
     function Orange(el) {
         this.el = el;
